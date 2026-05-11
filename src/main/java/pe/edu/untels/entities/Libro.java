7c3c647ea7b66a8b453e0214d3547062450eddf2
nick ronald <nickrando09@gmail.com>
2026-05-11 17:35:16 -0500

    Commit libro entity

---
+++ src/main/java/pe/edu/untels/entities/Libro.java
@@ -0,0 +1,86 @@
+package pe.edu.untels.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Libro")
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idLibro;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "autor", length = 100, nullable = false)
+    private String autor;
+
+    @Column(name = "isbn", length = 20, unique = true)
+    private String isbn;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @Column(name = "descripcion", length = 500)
+    private String descripcion;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String autor, String isbn, int stock, String descripcion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.stock = stock;
+        this.descripcion = descripcion;
+    }
+
+    public int getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(int idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
