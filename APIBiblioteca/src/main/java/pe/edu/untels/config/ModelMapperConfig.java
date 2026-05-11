2f01b45bd46f20e2e91e90dfa47ea9cdaa2c29e6
Cesar Salazar <2213110208@untels.edu.pe>
2026-05-11 11:33:05 -0500

    Configurar ModelMapper para mapeo de DTOs en biblioteca

--- APIBiblioteca/src/main/java/pe/edu/untels/config/ModelMapperConfig.java
+++ APIBiblioteca/src/main/java/pe/edu/untels/config/ModelMapperConfig.java
@@ -3,7 +3,9 @@ package pe.edu.untels.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+/**
+ * Configuración de ModelMapper para mapeo de DTOs en el sistema de biblioteca.
+ */
 @Configuration
 public class ModelMapperConfig {
 
