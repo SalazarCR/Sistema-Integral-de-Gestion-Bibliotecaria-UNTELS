6ef9c4fb4f101cdc143ca48c172b6a9bdfed681f
nick ronald <nickrando09@gmail.com>
2026-05-11 18:06:33 -0500

    Commit controller

---
+++ src/main/java/pe/edu/untels/controllers/LibroController.java
@@ -0,0 +1,10 @@
+package pe.edu.untels.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/libros")
+public class LibroController {
+
+}
