e071aa196c5e4b148661ba45b72bad3655631212
Carlos Curo <2113010682@untels.edu.pe>
2026-05-11 15:36:27 -0500

    Commit repository

---
+++ APIBiblioteca/src/main/java/pe/edu/untels/repositories/StudentRepository.java
@@ -0,0 +1,23 @@
+package pe.edu.untels.repositories;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.untels.entities.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByDni(String dni);
+
+    Optional<Student> findByCodigo(String codigo);
+
+    List<Student> findByEstado(Boolean estado);
+
+    List<Student> findByCarreraId(Long carreraId);
+
+    Page<Student> findAll(Pageable pageable);
+}
