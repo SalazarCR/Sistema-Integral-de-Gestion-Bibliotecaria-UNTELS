f6fa019e707a1df0132a0dd9ffe9368d4b47fc72
nick ronald <nickrando09@gmail.com>
2026-05-11 18:10:58 -0500

    Commit endpoint

--- src/main/java/pe/edu/untels/controllers/LibroController.java
+++ src/main/java/pe/edu/untels/controllers/LibroController.java
@@ -1,10 +1,26 @@
 package pe.edu.untels.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.untels.dtos.LibroDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/libros")
 public class LibroController {
 
+    @GetMapping
+    public List<LibroDTO> listarLibros() {
+
+        List<LibroDTO> lista = new ArrayList<>();
+
+        return lista;
+    }
+
+    @PostMapping
+    public String registrarLibro(@RequestBody LibroDTO dto) {
+
+        return "Libro registrado correctamente";
+    }
 }
