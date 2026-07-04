395e27c209e3ecfce56f8bdce4c90544b42e527f
Cesar Salazar <2213110208@untels.edu.pe>
2026-07-04 11:11:13 -0500

    actualizar backend1.1

--- Biblioteca-FrontEnd/FrontBiblioteca/demoFrontIS/src/app/components/homecomponent/homecomponent.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-homecomponent',
-  imports: [],
-  templateUrl: './homecomponent.html',
-  styleUrl: './homecomponent.css',
-})
-export class Homecomponent {}
--- Biblioteca-FrontEnd/FrontBiblioteca/demoFrontIS/src/app/components/proveedorcomponent/proveedor-listar/proveedor-listar.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-proveedor-listar',
-  imports: [],
-  templateUrl: './proveedor-listar.html',
-  styleUrl: './proveedor-listar.css',
-})
-export class ProveedorListar {}
